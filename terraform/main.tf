248c20ec1b337eb6fc24864256b593a900dc9005
HO4678 <123117953+HO4678@users.noreply.github.com>
2025-10-07 19:58:58 -0400

    Update main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "eks_sg" {
+  name        = "shopez-eks-sg"
+  vpc_id      = aws_vpc.main.id
+  description = "Allow internal and EKS traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "shopez-eks-sg"
+  }
+}
